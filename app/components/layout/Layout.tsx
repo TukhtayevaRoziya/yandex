cf6935d9973dcd7c6cf2aea4a9b644b1c6485c8d
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-10-12 20:50:08 +0500

    change

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -1,30 +1,48 @@
+/* eslint-disable @next/next/no-before-interactive-script-outside-document */
 import Head from 'next/head'
-import { FC, useEffect, useState } from 'react'
+import Script from 'next/script'
 
-interface ILayout{
-  children: any,
+import { FC, useEffect, useState } from 'react'
+import FavIcon from '../../assets/images/preloader.png'
+
+interface ILayout {
+  children: any
   title: string
 }
 
-const Layout:FC<ILayout> = ({children, title}) => {
+const Layout: FC<ILayout> = ({ children, title }) => {
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     setIsLoading(true)
     const timeout = setTimeout(() => {
       setIsLoading(false)
-    }, 4000);
+    }, 4000)
 
-    return ()=>{
+    return () => {
       clearTimeout(timeout)
     }
-  }, []);
+  }, [])
 
   return (
     <div>
-     <Head>
-      <title>{title} | Yandex Taxi Map</title>
+      <Head>
+        <title>{title} | Yandex Taxi Map</title>
+        <meta name="description" content="Taxi Api" />
+        <link rel="shutcut icon" href={FavIcon.src} type="image/png" />
       </Head>
+      <Script
+        strategy="beforeInteractive"
+        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.MAP_KEY}&libraries=places`}
+      />
+      <div
+        style={{
+          maxWidth: 480,
+        }}
+        className="mx-auto overflow-hidden relative"
+      >
+        {isLoading ? <Loader /> : children}
+      </div>
     </div>
   )
 }
---
+++ app/components/ui/Loader.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import imgs from '../../assets/images/preloader.png'
+
+const Loader = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Loader
